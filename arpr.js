b04e561348b3dc0295738a4e6c88d273e40f8e67
JUMTBOX <yjey12@naver.com>
2023-01-30 14:50:37 +0900

    Css 실습코드 추가

---
+++ arpr.js
@@ -0,0 +1,14 @@
+import styled-components from react;
+import material-ui from @muimaterial
+
+
+const iwannagohome = () => {
+    let answer = {...current}
+    if(answer === undefined){
+        console.log(Error)
+    }else{
+        console.log("정상작동")
+    }
+}
+
+export default iwannagohome;
